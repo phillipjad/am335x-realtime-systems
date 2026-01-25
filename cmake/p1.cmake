53ed8615a836fba63d9d50b3c6affcbae7855778
Phillip Miavelstuck <phillipjad@gmail.com>
2026-01-24 22:40:21 -0500

    Add cmake logic and add skeleton for project 1

---
+++ cmake/p1.cmake
@@ -0,0 +1,9 @@
+project(p1 C)
+
+# Add executable with source files from projects/project1/src/
+file(GLOB P1_SOURCES "${CMAKE_SOURCE_DIR}/projects/project1/src/*.c")
+
+add_executable(p1 ${P1_SOURCES})
+
+# Add include directories if needed
+target_include_directories(p1 PRIVATE "${CMAKE_SOURCE_DIR}/projects/project1/src")
---
+++ projects/project1/src/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello world from p1!\n");
+    return 0;
+}
