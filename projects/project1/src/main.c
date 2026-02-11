e34d57cdda97ca0a614fc21969aee14605b2f99a
Phillip Miavelstuck <phillipjad@gmail.com>
2026-02-11 18:06:06 -0500

    Add clang-format to pre-commit config and

--- projects/project1/src/main.c
+++ projects/project1/src/main.c
@@ -1,14 +1,15 @@
+#include <stdint.h>
 #include <stdio.h>
 
-int main(void) {
-	#if defined(DEBUG)
-    		printf("DEBUG MODE ENABLED\n");
-	#elif defined(NDEBUG)
-		printf("RELEASE MODE ENABLED\n");
-	#else
-		printf("NO MODE SELECTED\n");
-	#endif
-
-    printf("Hello world from p1!\n");
-    return 0;
+int32_t main(void) {
+#if defined(DEBUG)
+	printf("DEBUG MODE ENABLED\n");
+#elif defined(NDEBUG)
+	printf("RELEASE MODE ENABLED\n");
+#else
+	printf("NO MODE SELECTED\n");
+	exit(1);
+#endif
+	printf("Hello world from p1!\n");
+	return 0;
 }
