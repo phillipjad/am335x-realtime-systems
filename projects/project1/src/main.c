0588901f29f0bc4eb46177bbe6c4cb8a3ee224f1
Phillip Miavelstuck <phillipjad@gmail.com>
2026-02-11 22:05:10 -0500

    Integrate new modules into main.c to create minimal working executable

--- projects/project1/src/main.c
+++ projects/project1/src/main.c
@@ -1,15 +1,37 @@
-#include <stdint.h>
-#include <stdio.h>
+#include <unistd.h>
+
+/* Local project includes after system libraries */
+#include "logger.h"
+#include "project_constants.h"
+#include "project_types.h"
+#include "signal_handler.h"
+#include "traffic_logic.h"
+
+static void application_init(void) {
+	int32_t result = register_signal_handlers();
+	if (result != STATUS_SUCCESS) {
+		LOG_AND_EXIT("Failed to register application signal handlers. Status code: %d", result);
+	}
+	LOG("Initialized application");
+}
 
 int32_t main(void) {
 #if defined(DEBUG)
-	printf("DEBUG MODE ENABLED\n");
+	LOG("DEBUG MODE ENABLED");
 #elif defined(NDEBUG)
-	printf("RELEASE MODE ENABLED\n");
+	LOG("RELEASE MODE ENABLED");
 #else
-	printf("NO MODE SELECTED\n");
-	exit(1);
+	LOG_AND_EXIT("No release mode detected!");
 #endif
-	printf("Hello world from p1!\n");
-	return 0;
+
+	/* If initialization fails we fail-fast so no need for a return value */
+	application_init();
+
+	while (is_shutdown_requested() == 0) {
+		run_traffic_signal();
+		(void)sleep(MAIN_THREAD_SLEEP_S);
+	}
+
+	LOG("Starting application shutdown sequence...");
+	handle_shutdown();
 }
