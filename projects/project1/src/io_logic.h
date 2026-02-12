c12131f348078abf7c2b7eeb8dcdde1855795095
Phillip Miavelstuck <phillipjad@gmail.com>
2026-02-11 22:01:08 -0500

    Add io_logic module skeleton

---
+++ projects/project1/src/io_logic.h
@@ -0,0 +1,13 @@
+#ifndef IO_LOGIC_H
+#define IO_LOGIC_H
+#include "project_types.h"
+
+/**
+ * @brief Currently does nothing
+ *
+ * @param[in] pin Unused
+ * @param[in] value Unused
+ */
+void signal_gpio(uint16_t pin, uint8_t value);
+
+#endif
