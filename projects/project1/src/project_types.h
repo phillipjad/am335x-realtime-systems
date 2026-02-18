454abf7f266ec5450a7602102e59e7bdc3a52e6f
Phillip Miavelstuck <phillipjad@gmail.com>
2026-02-17 23:23:58 -0500

    Add float value typedefs and use seconds for green light duration

--- projects/project1/src/project_types.h
+++ projects/project1/src/project_types.h
@@ -8,6 +8,10 @@
 #define STATUS_SUCCESS (0)
 #define STATUS_FAIL (-1)
 
+typedef double float64_t;
+
+typedef float float32_t;
+
 typedef struct {
 	uint8_t green_light_ns;  /**< Pin for green light on north/south */
 	uint8_t green_light_ew;  /**< Pin for green light on east/west */
@@ -18,7 +22,7 @@ typedef struct {
 } gpio_pin_layout_t;
 
 typedef struct {
-	uint16_t green_light_duration_m;
+	uint16_t green_light_duration_s;
 	gpio_pin_layout_t pin_layout;
 } configuration_items_t;
 
