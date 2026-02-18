4cdb9af89b4460d0db4f451976a98db2fa4c9c03
Phillip Miavelstuck <phillipjad@gmail.com>
2026-02-17 23:08:59 -0500

    Enhance user_input module
    - Add missing docstrings and function decorators
    - Add functions to parse string user input into uint{8,16}_t data

--- projects/project1/src/utils/user_input.c
+++ projects/project1/src/utils/user_input.c
@@ -1,12 +1,63 @@
 #include "user_input.h"
 
+#include <errno.h>
 #include <stdio.h>
+#include <string.h>
 
 /* Local project includes after system libraries */
 #include "logger.h"
 
+#define BASE_DEC (10)
+
+/*--------------------------------------
+ * Function: get_user_input
+ *--------------------------------------*/
 int32_t get_user_input(char *input_buffer, size_t input_buffer_len, const char *prompt) {
 	LOG_WITHOUT_NEWLINE("%s: ", prompt);
 	char *return_val = fgets(input_buffer, input_buffer_len, stdin);
 	return return_val != NULL ? STATUS_SUCCESS : STATUS_FAIL;
+}
+
+/*--------------------------------------
+ * Function: parse_input_to_uint8
+ *--------------------------------------*/
+int32_t parse_input_to_uint8(const char *input_buffer, uint8_t *output) {
+	char *endptr = NULL;
+	errno = 0;
+	int64_t value = strtol(input_buffer, &endptr, BASE_DEC);
+	if (errno != 0) {
+		LOG("Experienced error while parsing user input to uint8_t: %s", strerror(errno));
+		return STATUS_FAIL;
+	}
+
+	if (value > UINT8_MAX || value < 0) {
+		LOG("User input is out of bounds for uint8_t: %ld not within range 0 <= X <= %d", value, UINT8_MAX);
+		return STATUS_FAIL;
+	}
+
+	*output = (uint8_t)value;
+
+	return STATUS_SUCCESS;
+}
+
+/*--------------------------------------
+ * Function: parse_input_to_uint16
+ *--------------------------------------*/
+int32_t parse_input_to_uint16(const char *input_buffer, uint16_t *output) {
+	char *endptr = NULL;
+	errno = 0;
+	int64_t value = strtol(input_buffer, &endptr, BASE_DEC);
+	if (errno != 0) {
+		LOG("Experienced error while parsing user input to uint16_t: %s", strerror(errno));
+		return STATUS_FAIL;
+	}
+
+	if (value > UINT16_MAX || value < 0) {
+		LOG("User input is out of bounds for uint16_t: %ld not within range 0 <= X <= %d", value, UINT16_MAX);
+		return STATUS_FAIL;
+	}
+
+	*output = (uint16_t)value;
+
+	return STATUS_SUCCESS;
 }
--- projects/project1/src/utils/user_input.h
+++ projects/project1/src/utils/user_input.h
@@ -12,4 +12,22 @@
  */
 int32_t get_user_input(char *input_buffer, size_t input_buffer_len, const char *prompt);
 
+/**
+ * @brief Parses the string in \p input_buffer into a uint16_t and stores in \p output
+ *
+ * @param[in] input_buffer A buffer to save the user response into
+ * @param[out] output A uint16_t value to store the parsed integer value of \p input_buffer
+ * @return int32_t Returns STATUS_SUCCESS on success and STATUS_FAIL on failure
+ */
+int32_t parse_input_to_uint8(const char *input_buffer, uint8_t *output);
+
+/**
+ * @brief Parses the string in \p input_buffer into a uint16_t and stores in \p output
+ *
+ * @param[in] input_buffer A buffer to save the user response into
+ * @param[out] output A uint16_t value to store the parsed integer value of \p input_buffer
+ * @return int32_t Returns STATUS_SUCCESS on success and STATUS_FAIL on failure
+ */
+int32_t parse_input_to_uint16(const char *input_buffer, uint16_t *output);
+
 #endif /* USER_INPUT_H */
