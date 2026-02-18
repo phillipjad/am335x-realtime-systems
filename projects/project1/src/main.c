851463898d4f4b4430aa4432e26d22b8405c4ce1
Phillip Miavelstuck <phillipjad@gmail.com>
2026-02-17 23:10:14 -0500

    Update main.c with user input logic
    - Add get_user_configuration_items function
    - Simplify main() function

--- projects/project1/src/main.c
+++ projects/project1/src/main.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <unistd.h>
 
 /* Local project includes after system libraries */
@@ -6,7 +7,23 @@
 #include "project_types.h"
 #include "signal_handler.h"
 #include "traffic_logic.h"
+#include "user_input.h"
 
+#define USER_INPUT_MAX_LEN (1024U)
+
+#define NUM_UTILIZED_PINS (6U)
+
+/* Struct for parsing user input for pins */
+typedef struct {
+	char *pin_string;
+	uint8_t *pin_pointer;
+} pin_io_struct_t;
+
+static configuration_items_t user_config = { 0 };
+
+/*--------------------------------------
+ * Static Function: application_init
+ *--------------------------------------*/
 static void application_init(void) {
 	int32_t result = register_signal_handlers();
 	if (result != STATUS_SUCCESS) {
@@ -15,7 +32,10 @@ static void application_init(void) {
 	LOG("Initialized application");
 }
 
-int32_t main(void) {
+/*--------------------------------------
+ * Static Function: log_mode
+ *--------------------------------------*/
+static void log_mode(void) {
 #if defined(DEBUG)
 	LOG("DEBUG MODE ENABLED");
 #elif defined(NDEBUG)
@@ -23,9 +43,62 @@ int32_t main(void) {
 #else
 	LOG_AND_EXIT("No release mode detected!");
 #endif
+}
+
+/*--------------------------------------
+ * Static Function: get_user_configuration_items
+ *--------------------------------------*/
+static void get_user_configuration_items(void) {
+	char input_buffer[USER_INPUT_MAX_LEN] = { 0 };
+
+	/* Get green light duration */
+	int32_t result = get_user_input(input_buffer, USER_INPUT_MAX_LEN, "How many minutes should green lights last");
+	if (result != STATUS_SUCCESS) {
+		LOG_AND_EXIT("Failed to get user input for green light duration");
+	}
+	result = parse_input_to_uint16(input_buffer, &user_config.green_light_duration_m);
+	if (result != STATUS_SUCCESS) {
+		LOG_AND_EXIT("Failed to parse green light duration string to int");
+	}
+	(void)memset(input_buffer, 0, sizeof(input_buffer));
+
+
+#ifdef NDEBUG
+	/* If built in RELEASE mode then we should get user input for pin layout from stdio */
+	gpio_pin_layout_t *pin_layout = &user_config.pin_layout;
+	pin_io_struct_t pin_info[NUM_UTILIZED_PINS] = { { .pin_string = "North/South green light",
+		                                            .pin_pointer = &pin_layout->green_light_ns },
+		{ .pin_string = "North/South yellow light", .pin_pointer = &pin_layout->yellow_light_ns },
+		{ .pin_string = "North/South red light", .pin_pointer = &pin_layout->red_light_ns },
+		{ .pin_string = "East/West green light", .pin_pointer = &pin_layout->green_light_ew },
+		{ .pin_string = "East/West yellow light", .pin_pointer = &pin_layout->yellow_light_ew },
+		{ .pin_string = "East/West red light", .pin_pointer = &pin_layout->red_light_ew } };
+	for (uint8_t ii = 0U; ii < NUM_UTILIZED_PINS; ++ii) {
+		pin_io_struct_t *io_option = &pin_info[ii];
+		char prompt[255] = { 0 };
+		(void)snprintf(prompt, 255, "Which pin should be used for %s", io_option->pin_string);
+		result = get_user_input(input_buffer, USER_INPUT_MAX_LEN, prompt);
+		if (result != STATUS_SUCCESS) {
+			LOG_AND_EXIT("Failed to get user input for %s pin", io_option->pin_string);
+		}
+		result = parse_input_to_uint8(input_buffer, io_option->pin_pointer);
+		if (result != STATUS_SUCCESS) {
+			LOG_AND_EXIT("Failed to parse user input for %s pin", pin_info->pin_string);
+		}
+		(void)memset(input_buffer, 0, sizeof(input_buffer));
+	}
+#endif /* NDEBUG */
+}
+
+/* Application entrypoint */
+int32_t main(void) {
+	/* Log the mode that the binary was compiled with */
+	log_mode();
 
 	/* If initialization fails we fail-fast so no need for a return value */
 	application_init();
+
+	get_user_configuration_items();
 
 	while (is_shutdown_requested() == 0) {
 		run_traffic_signal();
--- projects/project1/src/project_types.h
+++ projects/project1/src/project_types.h
@@ -8,4 +8,18 @@
 #define STATUS_SUCCESS (0)
 #define STATUS_FAIL (-1)
 
+typedef struct {
+	uint8_t green_light_ns;  /**< Pin for green light on north/south */
+	uint8_t green_light_ew;  /**< Pin for green light on east/west */
+	uint8_t yellow_light_ns; /**< Pin for yellow light on north/south */
+	uint8_t yellow_light_ew; /**< Pin for yellow light on east/west */
+	uint8_t red_light_ns;    /**< Pin for red light on north/south */
+	uint8_t red_light_ew;    /**< Pin for red light on east/west */
+} gpio_pin_layout_t;
+
+typedef struct {
+	uint16_t green_light_duration_m;
+	gpio_pin_layout_t pin_layout;
+} configuration_items_t;
+
 #endif /* PROJECT_TYPES_H */
