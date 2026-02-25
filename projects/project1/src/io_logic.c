2fa8f0c01c1051fc98d0c8965e826795b9f9f5bc
Phillip Miavelstuck <phillipjad@gmail.com>
2026-02-24 22:26:57 -0500

    Improve robustness of io_logic module
    - Ensure that GPIO files exist before writing where applicable
    - Unexport prior to exporting in case GPIO folder exists from before

--- projects/project1/src/io_logic.c
+++ projects/project1/src/io_logic.c
@@ -1,7 +1,8 @@
 #include "io_logic.h"
 
-#include <fcntl.h>
 #include <stdio.h>
+#include <time.h>
+#include <unistd.h>
 
 #include "logger.h"
 #include "project_constants.h"
@@ -10,10 +11,35 @@
 #define SYSFS_GPIO_PATH ("/sys/class/gpio")
 /** Path for GPIO export */
 #define SYSFS_GPIO_EXPORT_PATH ("/sys/class/gpio/export")
+/** Path for GPIO unexport */
+#define SYSFS_GPIO_UNEXPORT_PATH ("/sys/class/gpio/unexport")
 /** Maximum buffer size for sysfs gpio-related writing and reading */
 #define SYSFS_GPIO_MAX_BUFFER_SIZE (64U)
 /** Out direction for GPIO */
 #define SYSFS_GPIO_DIR_OUT ("out")
+/** The number of times to attempt to open GPIO file */
+#define SYSFS_GPIO_MAX_FILE_POLL_ATTEMPTS (3U)
+/** A quarter of a second represented in nanoseconds */
+#define QUARTER_SECOND_AS_NSEC (250000000L)
+
+static bool wait_for_file(const char *path) {
+	FILE *fp = fopen(path, "w");
+	uint8_t attempts = 0U;
+	while ((fp == NULL) && (attempts < SYSFS_GPIO_MAX_FILE_POLL_ATTEMPTS)) {
+		++attempts;
+		/* If file still isn't accessible on disk, sleep and reattempt access */
+		struct timespec timer = { 0 };
+		timer.tv_nsec = QUARTER_SECOND_AS_NSEC;
+		nanosleep(&timer, NULL);
+		fp = fopen(path, "w");
+	}
+	if (fp != NULL) {
+		(void)fclose(fp);
+		return true;
+	} else {
+		return false;
+	}
+}
 
 static void write_to_file(const char *path, const char *value) {
 	/* If anything in this function fails then we should exit catastrophically */
@@ -37,9 +63,24 @@ static void export_gpio(uint8_t gpio) {
 	write_to_file(SYSFS_GPIO_EXPORT_PATH, gpio_pin_as_string);
 }
 
+static void unexport_gpio(uint8_t gpio) {
+	char gpio_pin_as_string[SYSFS_GPIO_MAX_BUFFER_SIZE + 1U] = { 0 };
+	(void)snprintf(gpio_pin_as_string, SYSFS_GPIO_MAX_BUFFER_SIZE, "%u", gpio);
+	/* We want to ignore any errors received here, this unexport is best effort just so exporting always succeeds */
+	FILE *fp = fopen(SYSFS_GPIO_UNEXPORT_PATH, "w");
+	if (fp != NULL) {
+		(void)fprintf(fp, "%s", gpio_pin_as_string);
+		(void)fclose(fp);
+	}
+}
+
 static void set_gpio_dir(uint8_t gpio, const char *dir) {
 	char gpio_direction_path[MAX_FILE_PATH_LENGTH + 1U] = { 0 };
 	(void)snprintf(gpio_direction_path, MAX_FILE_PATH_LENGTH, "%s/gpio%u/direction", SYSFS_GPIO_PATH, gpio);
+	/* Ensure that direction file exists */
+	if (!wait_for_file(gpio_direction_path)) {
+		LOG_AND_EXIT("Timed out waiting for direction file at %s", gpio_direction_path);
+	}
 	write_to_file(gpio_direction_path, dir);
 }
 
@@ -52,6 +93,7 @@ static void gpio_write(uint8_t gpio, int8_t value) {
 }
 
 void signal_gpio(uint8_t gpio_pin, int8_t value) {
+	unexport_gpio(gpio_pin);
 	export_gpio(gpio_pin);
 	set_gpio_dir(gpio_pin, SYSFS_GPIO_DIR_OUT);
 	gpio_write(gpio_pin, value);
