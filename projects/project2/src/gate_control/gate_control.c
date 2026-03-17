bab39da3562d326bde7b7b78204b7df018cb18e8
Phillip Miavelstuck <phillipjad@gmail.com>
2026-03-17 12:48:55 -0400

    Refactor gate control thread to improve state handling and mutex usage

--- projects/project2/src/gate_control/gate_control.c
+++ projects/project2/src/gate_control/gate_control.c
@@ -5,69 +5,61 @@
 
 /* Local project includes after system libraries */
 #include "logger.h"
-#include "project_constants.h"
 #include "project_types.h"
 #include "servo_controller.h"
+
+static global_values_t *shared_info = NULL;
+
+static void handle_gate_logic(void) {
+	/* We want to track what the last state we acted on is */
+	static state_t last_operated_state = STATE_INVALID;
+	/* Grab state snapshot */
+	pthread_mutex_lock(&shared_info->mutex);
+	state_t current_state = shared_info->current_state;
+	// While state is same as initializing state, wait on cv
+	while ((current_state == last_operated_state) && (!atomic_load(&shared_info->is_shutdown_requested))) {
+		pthread_cond_wait(&shared_info->cv, &shared_info->mutex);
+	}
+	/* Grab updated state snapshot while we have the mutex */
+	current_state = shared_info->current_state;
+	pthread_mutex_unlock(&shared_info->mutex);
+
+	// Check if shutdown requested or state changed from idle
+	if (!atomic_load(&shared_info->is_shutdown_requested)) {
+		if (current_state == STATE_IDLE) {
+			servo_raise();
+			LOG("Gate read IDLE state, raising gate.");
+		} else if (current_state == STATE_ACTIVE) {
+			servo_lower();
+			LOG("Gate read STATE_ACTIVE state, lowering gate.");
+		} else if (current_state == STATE_FAIL_SAFE) {
+			servo_lower();
+			LOG("Gate read FAIL_STATE state, lowering gate.");
+		} else if (current_state == STATE_CLEARING) {
+			LOG("Gate read CLEARING_STATE state");
+			// Sleep for 1 second as per requirements
+			sleep(1);
+			servo_raise();
+			LOG("Waited and raising gate.");
+			// Grab lock
+			return;
+		}
+		last_operated_state = current_state;
+	} else {
+		return;
+	}
+}
 
 /*--------------------------------------
  * Function: gate_control_thread_entry
  *--------------------------------------*/
 void *gate_control_thread_entry(void *arg) {
 	LOG("Starting gate control thread");
-	global_values_t *shared_info = (global_values_t *)arg;
-	// Starting state
-	state_t servo_state = STATE_IDLE;
-	// Grab lock
-	pthread_mutex_lock(&shared_info->mutex);
+	shared_info = (global_values_t *)arg;
 	// Assign current starting state
-	servo_state = shared_info->current_state;
 	while (!atomic_load(&shared_info->is_shutdown_requested)) {
-		// While state is same as initializing state, wait on cv
-		while (shared_info->current_state == servo_state && !atomic_load(&shared_info->is_shutdown_requested)) {
-			pthread_cond_wait(&shared_info->cv, &shared_info->mutex);
-		}
-		// Check if shutdown requested or state changed from idle
-		if (!atomic_load(&shared_info->is_shutdown_requested)) {
-			// Snapshot of state
-			state_t snapshot_state = shared_info->current_state;
-			// Update current servo state
-			servo_state = snapshot_state;
-			// Release lock
-			pthread_mutex_unlock(&shared_info->mutex);
-			if (snapshot_state == STATE_IDLE) {
-				servo_raise();
-				LOG("Gate read IDLE state, raising gate.");
-			} else if (snapshot_state == STATE_ACTIVE) {
-				servo_lower();
-				LOG("Gate read STATE_ACTIVE state, lowering gate.");
-			} else if (snapshot_state == STATE_FAIL_SAFE) {
-				servo_lower();
-				LOG("Gate read FAIL_STATE state, lowering gate.");
-			} else if (snapshot_state == STATE_CLEARING) {
-				LOG("Gate read CLEARING_STATE state");
-				// Sleep for 1 second as per requirements
-				sleep(1);
-				servo_raise();
-				LOG("Waited and raising gate.");
-				// Grab lock
-				pthread_mutex_lock(&shared_info->mutex);
-
-				// Check if state changed right before grabbing mutex
-				if (shared_info->current_state == STATE_CLEARING) {
-					shared_info->current_state = STATE_IDLE;
-					shared_info->current_direction = DIRECTION_NONE;
-					pthread_cond_broadcast(&shared_info->cv);
-				}
-				continue;
-			}
-			// Grab lock
-			pthread_mutex_lock(&shared_info->mutex);
-		} else {
-			break;
-		}
+		handle_gate_logic();
 	}
-	// Release lock
-	pthread_mutex_unlock(&shared_info->mutex);
 	LOG("Shutting down gate control thread");
 	return NULL;
 }
