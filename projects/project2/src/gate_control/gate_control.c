56b08a5cf4e301b43eee43b835b38a5950b94e48
Phillip Miavelstuck <phillipjad@gmail.com>
2026-03-17 22:45:10 -0400

    Enhance gate logic to track warning light deactivation time and implement a delay before raising the gate

--- projects/project2/src/gate_control/gate_control.c
+++ projects/project2/src/gate_control/gate_control.c
@@ -1,10 +1,12 @@
 #include "gate_control.h"
 
 #include <pthread.h>
+#include <time.h>
 #include <unistd.h>
 
 /* Local project includes after system libraries */
 #include "logger.h"
+#include "project_constants.h"
 #include "project_types.h"
 #include "servo_controller.h"
 
@@ -13,6 +15,8 @@ static global_values_t *shared_info = NULL;
 static void handle_gate_logic(void) {
 	/* We want to track what the last state we acted on is */
 	static state_t last_operated_state = STATE_INVALID;
+	/* We want to track the last time the warning lights were deactivated */
+	static struct timespec last_lights_off_time = { 0 };
 	/* Grab state snapshot */
 	pthread_mutex_lock(&shared_info->mutex);
 	state_t current_state = shared_info->current_state;
@@ -37,8 +41,25 @@ static void handle_gate_logic(void) {
 			LOG("Gate read FAIL_STATE state, lowering gate.");
 		} else if (current_state == STATE_CLEARING) {
 			LOG("Gate read CLEARING_STATE state");
-			// Sleep for 1 second as per requirements
-			sleep(1);
+			/* Get current time */
+			struct timespec curr_time = { 0 };
+			(void)clock_gettime(CLOCK_MONOTONIC_RAW, &curr_time);
+			struct timespec lights_off_time = { 0 };
+			/* Wait for warning light module to update the light off timestamp */
+			do {
+				pthread_mutex_lock(&shared_info->mutex);
+				lights_off_time = shared_info->lights_off_time;
+				pthread_mutex_unlock(&shared_info->mutex);
+
+				/* Sleep for 25 milliseconds between each query */
+				static const struct timespec delay_timer = { .tv_sec = 0, .tv_nsec = (25L * NSEC_PER_MSEC) };
+				(void)nanosleep(&delay_timer, NULL);
+			} while ((last_lights_off_time.tv_sec == lights_off_time.tv_sec) &&
+			    (last_lights_off_time.tv_nsec == lights_off_time.tv_nsec));
+			last_lights_off_time = lights_off_time;
+			/* Once warning lights are off we want to wait for one second to pass and then raise gate */
+			static const struct timespec one_second_delay = { .tv_sec = 1, .tv_nsec = 0 };
+			(void)nanosleep(&one_second_delay, NULL);
 			servo_raise();
 			LOG("Waited and raising gate.");
 			return;
